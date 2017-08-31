6b5006bf22ebe2836afb897c927d4468a28a394b
Basil Khan <basil.khan@vidyard.com>
2017-08-31 01:35:46 -0400

    feat(Model): 3D objects using model component

--- index.vr.js
+++ index.vr.js
@@ -4,68 +4,31 @@ import {
   asset,
   Pano,
   View,
-  Sphere, Box, Cylinder, Plane,
-  AmbientLight, DirectionalLight, PointLight, SpotLight
+  Model,
+  AmbientLight, DirectionalLight
 } from 'react-vr';
 
 export default class react_vr extends React.Component {
   render() {
     return (
       <View>
-        <SpotLight
-          style={{
-            transform: [{
-              translateZ: -5
-            }]
+        <Model 
+          source={{
+            obj: asset('Astronaut.obj')
           }}
-          distance={2}
-          decay={1}
+          style={{
+            color: 'orange',
+            transform: [{ translate: [0,-2, -5 ] }]
+          }}
+          lit
+          texture={asset('chess-world.jpg')}
+        />
+        <DirectionalLight
+          style={{ transform: [{ translateX: -1000 }]}}
+        />
+        <AmbientLight
+          intensity={0.4}
          />
-        <Sphere
-          lit
-          style={{
-            color: 'yellow',
-            transform: [{ 
-              translate: [-1.5, 0, -4 ] 
-            }]
-          }}
-          heightSegments={20}
-          widthSegments={20}
-        />
-        <Box
-          lit
-          style={{
-            color: 'blue',
-            transform: [{ 
-              translate: [0, 0, -6 ] 
-            }]
-          }}
-        />
-        <Cylinder
-          lit
-          radiusTop={0}
-          segments={20}
-          style={{
-            color: 'red',
-            transform: [{ 
-              translate: [1.5, 0, -4 ]  
-            }]
-          }}
-        />
-        <Plane
-          lit
-          dimWidth={4}
-          dimHeight={4}
-          style={{
-            color: 'green',
-            transform: [{ 
-              translate: [0, -0.5, -5] 
-            },
-            {
-              rotateX: -90
-            }]
-          }}
-        />
       </View>
     );
   }
