6baeb6990e9c0f1378003aaa270abbe696363c91
Basil Khan <basil.khan@vidyard.com>
2017-08-31 01:18:28 -0400

    feat(lighting): Ambient light with 4 objects

--- index.vr.js
+++ index.vr.js
@@ -3,27 +3,63 @@ import {
   AppRegistry,
   asset,
   Pano,
-  PointLight,
   View,
-  Sphere, 
-  Box, 
-  Cylinder,
-  Plane
+  Sphere, Box, Cylinder, Plane,
+  AmbientLight, DirectionalLight, PointLight, SpotLight
 } from 'react-vr';
 
 export default class react_vr extends React.Component {
   render() {
     return (
       <View>
+        <AmbientLight
+          intensity={2}
+          style={{ color: 'brown' }}
+         />
         <Sphere
-          style={{
-            color: 'lightblue',
-            transform: [{ translateZ: -2 }]
-          }}
           lit
-          texture={asset('mountains.jpg')}
+          style={{
+            color: 'yellow',
+            transform: [{ 
+              translate: [-1.5, 0, -4 ] 
+            }]
+          }}
           heightSegments={20}
           widthSegments={20}
+        />
+        <Box
+          lit
+          style={{
+            color: 'blue',
+            transform: [{ 
+              translate: [0, 0, -6 ] 
+            }]
+          }}
+        />
+        <Cylinder
+          lit
+          radiusTop={0}
+          segments={20}
+          style={{
+            color: 'red',
+            transform: [{ 
+              translate: [1.5, 0, -4 ]  
+            }]
+          }}
+        />
+        <Plane
+          lit
+          dimWidth={4}
+          dimHeight={4}
+          style={{
+            color: 'green',
+            transform: [{ 
+              translate: [0, -0.5, -5] 
+            },
+            {
+              rotateX: -90
+            }]
+          }}
         />
         <PointLight
           intensity={1}
